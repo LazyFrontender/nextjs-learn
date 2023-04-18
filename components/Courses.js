a0a7b4c696da98dcf003c2aa8f07eafa05aebc0b
Javohir Hasanov <javohirhasanov3303@gmail.com>
2023-04-18 20:56:23 +0500

    add alt

--- components/Courses.js
+++ components/Courses.js
@@ -6,14 +6,14 @@ export default function Courses({ course }) {
   return (
     <>
       <div>
-        <img src={image.data.attributes.url} alt="" />
+        <img src={image.data.attributes.url} alt={title} />
       </div>
       <h3>{title}</h3>
       <h4>{description}</h4>
       <h3>$ {price}</h3>
       <div>
         <h4>{author}</h4>
-        <img src={avatar.data.attributes.url} alt="" />
+        <img src={avatar.data.attributes.url} alt={title} />
       </div>
     </>
   );
